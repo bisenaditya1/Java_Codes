72a51c19f9271d093c308cc9c253ea305f6de560
bisenaditya1 <138757849+bisenaditya1@users.noreply.github.com>
2023-07-21 14:12:26 +0530

    Create Sort012.java

---
+++ Sort012.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Sort012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {0, 2, 1, 2, 0};
+		Arrays.sort(arr);
+		
+		for( int element : arr)
+			System.out.print(element + " ");
+		
+		
+	}
+
+}
